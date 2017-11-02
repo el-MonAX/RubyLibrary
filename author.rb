c753e2c791f2f474b0745c20d46a23a7ca83ccd7
el-MonAX <makc.mukhortov@gmail.com>
2017-11-03 00:35:36 +0200

    init repository

---
+++ author.rb
@@ -0,0 +1,12 @@
+class Author
+  attr_accessor :name, :biography
+
+  def initialize(name, biography)
+    @name = name
+    @biography = biography
+  end
+
+  def to_s
+    "Author: #{@name}.\nBiography: #{@biography}."
+  end
+end
---
+++ order.rb
@@ -0,0 +1,13 @@
+class Orde
+  attr_accessor :book, :reader, :date
+
+  def initialize(book, reader, date = Time.now)
+    @book = book
+    @reader = reader
+    @date = date
+  end
+
+  def to_s
+    "Book: '#{@book.title}' by #{@book.author.name}.\nReader: #{@reader.name} (#{@reader.email})\nOrder time: #{@date}."
+  end
+end
